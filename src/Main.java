1bceac3db50e698326979dc6ce9f17ec87a5e0b2
Abduvohid Toshtemirov <toshtemirovabduvohid77@gmail.com>
2023-12-26 22:29:39 +0500

    Wrote some codes in main, check it !

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello Javokhir aka, can you see my code?");
+        System.out.println("I wanna work with you, you're good Programmer !");
     }
 }
