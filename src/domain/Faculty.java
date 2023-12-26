f930da773da77f7fd906ca60f1b755a3bd33153f
javokhir200 <javokhirganiboyev2004@gmail.com>
2023-12-26 15:01:48 +0500

    domaindagi ma'lumot yangilandi

--- src/domain/Faculty.java
+++ src/domain/Faculty.java
@@ -1,4 +1,55 @@
 package domain;
 
 public class Faculty {
+    private int facultyId;
+    private String facultyName;
+    private int studentCount;
+    private int teacherCount;
+
+    public Faculty(int facultyId, String facultyName) {
+        this.facultyId = facultyId;
+        this.facultyName = facultyName;
+    }
+
+    public int getFacultyId() {
+        return facultyId;
+    }
+
+    public void setFacultyId(int facultyId) {
+        this.facultyId = facultyId;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+
+    public void setFacultyName(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public void setStudentCount(int studentCount) {
+        this.studentCount = studentCount;
+    }
+
+    public int getTeacherCount() {
+        return teacherCount;
+    }
+
+    public void setTeacherCount(int teacherCount) {
+        this.teacherCount = teacherCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "facultyId=" + facultyId +
+                ", facultyName='" + facultyName +
+                ", studentCount=" + studentCount +
+                ", teacherCount=" + teacherCount +
+                '}';
+    }
 }
--- src/domain/User.java
+++ src/domain/User.java
@@ -1,7 +1,27 @@
 package domain;
 
+import enums.UserStatusEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private int UserId;
+    private String fullName;
+    private int facultyId;
+    private int groupId;
+    private UserStatusEnum status;
+    private int gpa;
+    private List<Grade> GRADES = new ArrayList<>();
+
+    public User(int userId, String fullName, int facultyId, int groupId, UserStatusEnum status, int gpa) {
+        UserId = userId;
+        this.fullName = fullName;
+        this.facultyId = facultyId;
+        this.groupId = groupId;
+        this.status = status;
+        this.gpa = gpa;
+    }
 
     public void setUserId(int userId) {
         UserId = userId;
@@ -9,5 +29,53 @@ public class User {
 
     public int getUserId() {
         return UserId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getFacultyId() {
+        return facultyId;
+    }
+
+    public void setFacultyId(int facultyId) {
+        this.facultyId = facultyId;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public UserStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatusEnum status) {
+        this.status = status;
+    }
+
+    public int getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(int gpa) {
+        this.gpa = gpa;
+    }
+
+    public List<Grade> getGRADES() {
+        return GRADES;
+    }
+
+    public void setGRADES(Grade grade) {
+        GRADES.add(grade);
     }
 }
