5654c5bb1df8945edaf5cbc35db5e2ebea8bda57
Aziz Inayatov <a88840934@gmail.com>
2024-01-05 01:40:09 +0500

    .changed

--- src/service/impl/UserServiceImpl.java
+++ src/service/impl/UserServiceImpl.java
@@ -4,33 +4,47 @@ import domain.User;
 import enums.UserStatusEnum;
 import service.UserService;
 
+import java.util.Map;
 import java.util.Properties;
 
 public class UserServiceImpl implements UserService {
     @Override
     public User register(User user, String password) {
-        return  null;
+        if(user != null && password != null) {
+            USERS.put(user, user.getUserId());
+        }
+        return  user;
         //nullga tekshirib mapga qo'shib qoysa boldi
     }
 
     @Override
     public User login(String email, String password) {
         //nullga tekshirib mapdan userni olib qaytarvorsa boldi
+
         return null;
     }
 
     @Override
     public String delete(Integer userId) {
-        return null;//o'chirish
+        USERS.remove(userId);
+        return userId + " has successfully deleted";
+     //o'chirish
     }
 
     @Override
     public String changeStatus(Integer userId, UserStatusEnum newStatus) {
-        return null;
+        for (Map.Entry<User, Integer> userIntegerEntry : USERS.entrySet()) {
+            if(userId.equals(userIntegerEntry.getValue())) {
+                userIntegerEntry.getKey().setStatus(newStatus);
+            }
+        }
+
+        return "Successfully changed";
     }
 
     @Override
     public String changeEmail(Integer userId, String newEmail) {
+
         return null;
     }
 
@@ -45,6 +59,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void showAllUsers() {
+        for (Map.Entry<User, Integer> userIntegerEntry : USERS.entrySet()) {
+            System.out.println(userIntegerEntry.toString());
+        }
         //show method in here
     }
 }
